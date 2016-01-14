48a019a94c70a972beaa32b4373cf89154eabf87
Jeremy Dowell <jeremy@codevinsky.com>
2016-01-14 09:39:06 -0600

    Updating for new files and stuff

--- server.js
+++ server.js
@@ -10,10 +10,23 @@ var _ = require('lodash');
 var db = mongoose.connection;
 var path = require('path');
 
-var port = process.env.PORT ||  1337;
+var port = process.env.PORT || 1337;
 
-var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
-                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };   
+var options = {
+  server: {
+    socketOptions: {
+      keepAlive: 1,
+      connectTimeoutMS: 30000
+    }
+  },
+  replset: {
+    socketOptions: {
+      keepAlive: 1,
+      connectTimeoutMS: 30000
+    }
+  }
+};
+
 var mongooseUri = uriUtil.formatMongoose('mongodb://fwordapp:r3Dcastl3@ds035428.mongolab.com:35428/heroku_app26222746');
 console.log(mongooseUri);
 mongoose.connect(mongooseUri, options);
@@ -23,16 +36,22 @@ var SingleFuck = require('./lib/schemas/SingleFuck');
 var Author = require('./lib/schemas/Author');
 
 router.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://thefword.codevinsky.com');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-    next(); // make sure we go to the next routes and don't stop here
+  res.header('Access-Control-Allow-Origin', 'http://thefword.codevinsky.com');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next(); // make sure we go to the next routes and don't stop here
 });
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
   var now = Date.now();
-  Fuck.find({created: {$lt: now}}).sort({created: -1}).limit(1).exec(function(err, mostRecent) {
-    if(!err) {
+  Fuck.find({
+    created: {
+      $lt: now
+    }
+  }).sort({
+    created: -1
+  }).limit(1).exec(function(err, mostRecent) {
+    if (!err) {
       res.json(mostRecent[0]);
     } else {
       console.log(err);
@@ -42,9 +61,18 @@ router.get('/', function(req, res) {
 });
 router.get('/singleFucks', function(req, res) {
   var now = Date.now();
-  SingleFuck.find({created: {$lt: now}}).sort({created: -1}).limit(1).exec(function(err, mostRecent) {
-    if(!err) {
-      mostRecent[0].ngrams.push({term: 'Accumulated Fucks', tf: mostRecent[0].total});
+  SingleFuck.find({
+    created: {
+      $lt: now
+    }
+  }).sort({
+    created: -1
+  }).limit(1).exec(function(err, mostRecent) {
+    if (!err) {
+      mostRecent[0].ngrams.push({
+        term: 'Accumulated Fucks',
+        tf: mostRecent[0].total
+      });
       res.json(mostRecent[0]);
     } else {
       console.log(err);
@@ -54,7 +82,7 @@ router.get('/singleFucks', function(req, res) {
 });
 router.get('/authors', function(req, res) {
   Author.find().exec(function(err, authors) {
-    if(!err) {
+    if (!err) {
       authors = _.first(authors, 1000);
       res.json(authors);
     } else {
@@ -65,9 +93,10 @@ router.get('/authors', function(req, res) {
 });
 
 app.use(bodyParser());
-app.use(function(req,res, next) {
+
+app.use(function(req, res, next) {
   console.log('headers:', req.headers.host);
-  if(!/codevinsky.com/.test(req.headers.host)) {
+  if (!/codevinsky.com/.test(req.headers.host)) {
     console.log('not coming from codevinsky.com... redirecting');
     res.redirect(301, 'http://thefword.codevinsky.com');
   } else {
